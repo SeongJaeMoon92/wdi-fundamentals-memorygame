1055c1e23b66b33d0f9f6df0f9f85c6e6b1ec1a1
seongjae Moon <seongjaemoon@seongjaes-MacBook-Pro.local>
2019-01-11 14:23:47 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,40 +8,47 @@ var cardFour = "king";*/
 
 // console.log("User flipped " + cardTwo);
 
-var cardsArray = ["queen", "queen", "king", "king"];
+var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cardsArray[0];
+// var cardOne = cards[0];
+// 	cardsInPlay.push(cardOne);
+// 	console.log("User flipped " + cardOne);
 
-cardsInPlay.push(cardOne);
+// 	var cardTwo = cards[2];
+// 	cardsInPlay.push(cardTwo);
+// 	console.log("User flipped " + cardTwo);
 
-console.log("User flipped " + cardOne);
-
-var cardTwo = cardsArray[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!")
-	} else {
-		alert("Sorry, try again.")
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+		} else {
+	console.log("Sorry, try again.");
 	}
 }
 
-// Now we'll want to add logic that checks for equality between the two cards in the cardsInPlay array.
-// Inside of the if statement you created in the last step, create an if...else statement that checks for equality 
-// between the two cards in the cardsInPlay array.
-// For the condition, check to see if the first card in the cardsInPlay array (cardsInPlay[0]) is equal to the second card in the cardsInPlay array.
-// HINT: You'll want to use the === operator here.
+var flipCard = function (cardId) {
 
-// If values are equal, execute an alert with the message "You found a match!"
-// alert("You found a match!");
-// If values are not equal, execute an alert with the message "Sorry, try again."
+	checkForMatch();
 
-// After saving your main.js file, open the index.html file in Sublime Text, right click, and open the project in Chrome. 
-//The alert "Sorry, try again." should pop up.
+	console.log("User flipped " + cards[cardId])
+	cardsInPlay.push(cards[cardId])
 
-// Test out storing different cards in the cardOne and cardTwo variables to make sure that everything is up and running!
+
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match!")
+		} else {
+			alert("Sorry, try again.")
+		}
+	}
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+
+
+
